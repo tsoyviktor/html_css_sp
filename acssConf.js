ade1401fb0f921e2ed4f2b7b0f31d836799a641a
Val Mars <valeriy.mars@gmail.com>
2017-12-05 23:24:24 -0800

    WIP

--- acssConf.js
+++ acssConf.js
@@ -2,11 +2,13 @@ module.exports = {
     'custom': {
         'light': 'rgba(0, 0, 0, 0.12) 0 1px 6px, rgba(0, 0, 0, 0.12) 0 1px 4px',
         'Bgc(light)': 'rgb(232, 232, 232)',
-        'Trs(default)': 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
+        'Trs(default)': 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+        'C(hsla)': 'rgba(0,0,0,.7)'
     },
     "classNames": [
         "Bxsh(light)",
-        "Trs(default)"
+        "Trs(default)",
+        "C(hsla)"
     ],
     'breakPoints': {
         'sm': '@media screen and (min-width: 380px)',
